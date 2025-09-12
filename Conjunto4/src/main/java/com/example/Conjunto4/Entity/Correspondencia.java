dcdcc8fbaf62e4d2afdd2075b4749d4c2e3a7b6d
Paula Sepulveda <paula06sepulveda@gmail.com>
2025-09-12 16:05:04 -0500

    nueva carpeta

---
+++ Conjunto4/src/main/java/com/example/Conjunto4/Entity/Correspondencia.java
@@ -0,0 +1,25 @@
+package com.example.Conjunto4.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "correspondencias")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Correspondencia {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String descripcion;
+    private String fechaRecepcion;
+    private String entregadoA;
+
+    @ManyToOne
+    @JoinColumn(name = "apartamento_id")
+    private Apartamento apartamento;
+}
---
+++ Conjunto4/src/main/java/com/example/Conjunto4/Entity/Residente.java
@@ -0,0 +1,27 @@
+package com.example.Conjunto4.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "residentes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Residente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private String apellido;
+    private String documento;
+    private String telefono;
+    private String email;
+
+    @ManyToOne
+    @JoinColumn(name = "apartamento_id")
+    private Apartamento apartamento;
+}
