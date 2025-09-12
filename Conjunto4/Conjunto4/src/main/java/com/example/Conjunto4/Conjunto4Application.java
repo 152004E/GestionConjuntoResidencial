7e6864bae5d9e5fed0aba10b8918ee621552792f
Paula Sepulveda <paula06sepulveda@gmail.com>
2025-09-12 15:43:08 -0500

    agregando carpeta

---
+++ Conjunto4/Conjunto4/src/main/java/com/example/Conjunto4/Conjunto4Application.java
@@ -0,0 +1,13 @@
+package com.example.Conjunto4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Conjunto4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Conjunto4Application.class, args);
+	}
+
+}
---
+++ Conjunto4/Conjunto4/src/test/java/com/example/Conjunto4/Conjunto4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Conjunto4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Conjunto4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
