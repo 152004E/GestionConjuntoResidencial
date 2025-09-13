7201010ee38c9124f5cce1f44537f31e0b3891d2
Paula Sepulveda <paula06sepulveda@gmail.com>
2025-09-13 06:38:15 -0500

    listo

--- Conjunto4/src/main/java/com/example/Conjunto4/Repository/SolicitudRepository.java
+++ Conjunto4/src/main/java/com/example/Conjunto4/Repository/SolicitudRepository.java
@@ -1,5 +1,9 @@
 package com.example.Conjunto4.Repository;
 
-public class SolicitudRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.Conjunto4.Entity.Solicitud;
+
+public interface SolicitudRepository  extends JpaRepository<Solicitud, Long> {
 
 }
